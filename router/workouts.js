bf8bdc7b5792d64c0e67bb84011d088482079b23
Falcao1992 <eduardo.marselle@gmail.com>
2020-08-27 20:49:05 +0200

    add route delete one details exercices

--- router/workouts.js
+++ router/workouts.js
@@ -26,6 +26,26 @@ router.get('/:userId/workouts', async (req, res) => {
     res.status(200).send(workouts)
 })
 
+// GET One workout (details exercises and default exercises name) associate at one userId
+router.get('/:userId/workouts/:id', async (req, res) => {
+    const {userId, id} = req.params
+    const OneWorkout = await Workout.findOne({
+        where: {userId, id},
+        include: [
+            {
+                model: DetailsExercise,
+                include: [
+                    {
+                        model: DefaultExercise,
+                        attributes: ['name'],
+                    }
+                ]
+            }
+        ]
+    })
+    res.status(200).send(OneWorkout)
+})
+
 // Put Workouts associate at one User
 router.put('/:userId/workout/:id', async (req, res) => {
     const {userId, id} = req.params
@@ -39,11 +59,25 @@ router.put('/:userId/workout/:id', async (req, res) => {
 
 
 // Get all default exercises
-
 router.get('/defaultExercises', async (req, res) => {
     const allDefaultExercises = await DefaultExercise.findAll()
     res.status(200).send(allDefaultExercises)
 })
+
+// Get all default exercises
+router.post('/detailsExercise', async (req, res) => {
+    const {DefaultExerciseId, duration, number, series, WorkoutId} = req.body
+    const newDetailsExercise = await DetailsExercise.create({DefaultExerciseId, duration, number, series, WorkoutId })
+    res.status(200).send(newDetailsExercise)
+})
+
+// Get all default exercises
+router.delete('/detailsExercise/:id', async (req, res) => {
+    const { id } = req.params
+    await DetailsExercise.destroy({ where: { id } })
+    res.status(200).send(id)
+})
+
 
 /*
 // DELETE one user by ID
