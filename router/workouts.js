3d216b12cfc104a5ce16c804a0019f33e551c46c
Falcao1992 <eduardo.marselle@gmail.com>
2020-08-28 22:03:38 +0200

    refactor

--- router/workouts.js
+++ router/workouts.js
@@ -92,34 +92,4 @@ router.delete('/detailsExercise/:id', async (req, res) => {
     res.status(200).send(id)
 })
 
-
-/*
-// DELETE one user by ID
-router.delete('/:id', async (req, res) => {
-    const {id} = req.params
-    await User.destroy({where: {id}})
-    res.send(id)
-})
-
-// GET all users
-/*router.get('/', async (req, res) => {
-    const allUsers = await User.findAll()
-    res.send(allUsers)
-})
-// GET one user by ID
-router.get('/:id', async (req, res) => {
-    const {id} = req.params
-    const user = await User.findOne({where: {id}})
-    res.send(user)
-})
-// POST new user
-router.post('/', async (req, res) => {
-    const {firstName, lastName, size, weight, dateBirth, email} = req.body
-    const user = User.create({firstName, lastName, size, weight, dateBirth, email})
-    res.send(user)
-})
-
- */
-
-
 module.exports = router;
