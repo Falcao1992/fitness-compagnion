edc746e74619d4680f4a109314de3277e343cadb
Falcao1992 <eduardo.marselle@gmail.com>
2020-09-20 01:53:45 +0200

    change config

--- config/config.js
+++ config/config.js
@@ -15,12 +15,7 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "use_env_variable" :  "DATABASE_URL",
-    "username": process.env.HEROKU_USER,
-    "password": process.env.HEROKU_PASSWORD,
-    "database": process.env.HEROKU_DB,
-    "host": process.env.HEROKU_HOST,
-    "dialect": "mysql"
-    //postgres
+    //"use_env_variable" : "DATABASE_URL",
+    use_env_variable: 'DATABASE_URL',
   }
 }
