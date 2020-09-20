bc019ab31eb6ff38684708ae437c7673df63b0c2
Falcao1992 <eduardo.marselle@gmail.com>
2020-09-20 03:24:08 +0200

    change casse referencie user in workouts

--- migrations/20200817105815-create-workout.js
+++ migrations/20200817105815-create-workout.js
@@ -24,7 +24,7 @@ module.exports = {
             allowNull: false,
             type: Sequelize.INTEGER,
             references: {
-                model: 'Users',
+                model: 'User',
                 key: 'id',
             },
             // if userId is deleted so data workout is deleted too
