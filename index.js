413ccea52664d3f7aa9b477d86c4f232d9fe0f3c
Falcao1992 <eduardo.marselle@gmail.com>
2020-09-21 17:38:56 +0200

    add manage error 401 stats and editProfile's components

--- index.js
+++ index.js
@@ -2,31 +2,13 @@ const express = require ('express')
 const cors = require('cors');
 const app = express()
 
-//app.use(cors())
 app.use(cors({credentials: true, origin: 'https://fitness-companion.netlify.app'}));
-/*app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://fitness-companion.netlify.app"); // update to match the domain you will make the request frommm
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    console.log('dans le middleware cors')
-    next();
-});*/
-//app.use(cors({origin: ['http://localhost:5000', 'http://localhost:3000', 'https://fitness-companion.netlify.app']}));
-/*app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://fitness-companion.netlify.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});*/
-
 app.use(express.json());
 app.use(
     express.urlencoded({
         extended: true,
     }),
 );
-
-
 
 // Routes Users
 app.use('/user', require('./router/users'))
